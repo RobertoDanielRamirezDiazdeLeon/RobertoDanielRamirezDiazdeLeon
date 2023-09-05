b58378b253bae6c4350b933ae4e8a1583478eb5c
RobertoDanielRamirezDiazdeLeon <l21140727@queretaro.tecnm.mx>
2023-09-05 14:22:59 -0600

    Cambios2

--- Lenguaje.cs
+++ Lenguaje.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 /*
     Requerimiento 1: Mensajes del printf deben salir sin comillas
                      Incluir \n y \t como secuencias de escape
+    Requerimiento 2: Agregar el % al PorFactor
+                     Modifcar el valor de una variable con ++,--,+=,-=,*=,/=.%=
+    Requerimiento 3: Cada vez que se haga un match(Tipos.Identificador) verficar el
+                     uso de la variable
+                     Icremento(), Printf(), Factor() y usar getValor y Modifica
+                     Levantar una excepcion en scanf() cuando se capture un string
+    Requerimiento 4: Implemenar la ejecución del ELSE
 */
 
 namespace Sintaxis_2
@@ -36,7 +43,7 @@ namespace Sintaxis_2
             {
                 Variables();
             }
-            Main();
+            Main(true);
             Imprime();
         }
 
@@ -63,18 +70,16 @@ namespace Sintaxis_2
             }
             return false;
         }
-
         private void Modifica(string nombre, float nuevoValor)
         {
             foreach (Variable v in lista)
             {
-                if(v.getNombre() == nombre)
+                if (v.getNombre() == nombre)
                 {
                     v.setValor(nuevoValor);
                 }
             }
         }
-
         private float getValor(string nombre)
         {
             foreach (Variable v in lista)
@@ -86,7 +91,6 @@ namespace Sintaxis_2
             }
             return 0;
         }
-
         // Libreria -> #include<Identificador(.h)?>
         private void Libreria()
         {
@@ -146,60 +150,59 @@ namespace Sintaxis_2
             }
         }
         //BloqueInstrucciones -> { ListaInstrucciones ? }
-        private void BloqueInstrucciones()
+        private void BloqueInstrucciones(bool ejecuta)
         {
             match("{");
             if (getContenido() != "}")
             {
-                ListaInstrucciones();
+                ListaInstrucciones(ejecuta);
             }
             match("}");
         }
 
         //ListaInstrucciones -> Instruccion ListaInstrucciones?
-        private void ListaInstrucciones()
+        private void ListaInstrucciones(bool ejecuta)
         {
-            Instruccion();
+            Instruccion(ejecuta);
             if (getContenido() != "}")
             {
-                ListaInstrucciones();
+                ListaInstrucciones(ejecuta);
             }
         }
         //Instruccion -> Printf | Scanf | If | While | Do | For | Asignacion
-        private void Instruccion()
+        private void Instruccion(bool ejecuta)
         {
             if (getContenido() == "printf")
             {
-                Printf();
+                Printf(ejecuta);
             }
             else if (getContenido() == "scanf")
             {
-                Scanf();
+                Scanf(ejecuta);
+            }
+             else if (getContenido() == "if")
+            {
+                If(ejecuta);
             }
             else if (getContenido() == "while")
             {
-                While();
-            }
-            else if (getContenido() == "if")
-            {
-                If();
+                While(ejecuta);
             }
             else if (getContenido() == "do")
             {
-                Do();
+                Do(ejecuta);
             }
             else if (getContenido() == "for")
             {
-                For();
+                For(ejecuta);
             }
-            // ...
             else
             {
-                Asignacion();
+                Asignacion(ejecuta);
             }
         }
         //Asignacion -> identificador = Expresion;
-        private void Asignacion()
+        private void Asignacion(bool ejecuta)
         {
             if (!Existe(getContenido()))
             {
@@ -250,12 +253,15 @@ namespace Sintaxis_2
                 Expresion();
             }
             float resultado = stack.Pop();
-            log.WriteLine(" = "+resultado);
-            Modifica(variable,resultado);
+            log.WriteLine(" = " + resultado);
+            if (ejecuta)
+            {
+                Modifica(variable,resultado);
+            }
             match(";");
         }
         //While -> while(Condicion) BloqueInstrucciones | Instruccion
-        private void While()
+        private void While(bool ejecuta)
         {
             match("while");
             match("(");
@@ -263,25 +269,25 @@ namespace Sintaxis_2
             match(")");
             if (getContenido() == "{")
             {
-                BloqueInstrucciones();
+                BloqueInstrucciones(ejecuta);
             }
             else
             {
-                Instruccion();
+                Instruccion(ejecuta);
             }
 
         }
         //Do -> do BloqueInstrucciones | Instruccion while(Condicion)
-        private void Do()
+        private void Do(bool ejecuta)
         {
             match("do");
             if (getContenido() == "{")
             {
-                BloqueInstrucciones();
+                BloqueInstrucciones(ejecuta);
             }
             else
             {
-                Instruccion();
+                Instruccion(ejecuta);
             }
             match("while");
             match("(");
@@ -290,26 +296,26 @@ namespace Sintaxis_2
             match(";");
         }
         //For -> for(Asignacion Condicion; Incremento) BloqueInstrucciones | Instruccion
-        private void For()
+        private void For(bool ejecuta)
         {
             match("for");
             match("(");
-            Asignacion();
+            Asignacion(ejecuta);
             Condicion();
             match(";");
-            Incremento();
+            Incremento(ejecuta);
             match(")");
             if (getContenido() == "{")
             {
-                BloqueInstrucciones();
+                BloqueInstrucciones(ejecuta);
             }
             else
             {
-                Instruccion();
+                Instruccion(ejecuta);
             }
         }
         //Incremento -> Identificador ++ | --
-        private void Incremento()
+        private void Incremento(bool ejecuta)
         {
             if (!Existe(getContenido()))
             {
@@ -335,30 +341,31 @@ namespace Sintaxis_2
             float R1 = stack.Pop();
             float R2 = stack.Pop();
 
-            switch(operador)
+            switch (operador)
             {
-                case "==": return R2 == R1;
-                case ">" : return R2 > R1;
-                case ">=": return R2 >= R1;
-                case "<" : return R2 < R1;
-                case "<=": return R2 <= R1;
-                default  : return R2 != R1;
+                case "==" : return R2==R1;
+                case ">"  : return R2>R1;
+                case ">=" : return R2>=R1;
+                case "<"  : return R2<R1;
+                case "<=" : return R2<=R1;
+                default   : return R2!=R1;
             }
         }
         //If -> if (Condicion) BloqueInstrucciones | Instruccion (else BloqueInstrucciones | Instruccion)?
-        private void If()
+        private void If(bool ejecuta)
         {
             match("if");
             match("(");
-            Condicion();
+            bool evaluacion = Condicion() && ejecuta;
+            Console.WriteLine(evaluacion);
             match(")");
             if (getContenido() == "{")
             {
-                BloqueInstrucciones();
+                BloqueInstrucciones(evaluacion);
             }
             else
             {
-                Instruccion();
+                Instruccion(evaluacion);
             }
             if (getContenido() == "else")
             {
@@ -366,21 +373,24 @@ namespace Sintaxis_2
 
                 if (getContenido() == "{")
                 {
-                    BloqueInstrucciones();
+                    BloqueInstrucciones(ejecuta);
                 }
                 else
                 {
-                    Instruccion();
+                    Instruccion(ejecuta);
                 }
             }
 
         }
         //Printf -> printf(cadena(,Identificador)?);
-        private void Printf()
+        private void Printf(bool ejecuta)
         {
             match("printf");
             match("(");
-            Console.Write(getContenido());
+            if (ejecuta)
+            {
+                Console.Write(getContenido());
+            }
             match(Tipos.Cadena);
             if (getContenido() == ",")
             {
@@ -395,7 +405,7 @@ namespace Sintaxis_2
             match(";");
         }
         //Scanf -> scanf(cadena,&Identificador);
-        private void Scanf()
+        private void Scanf(bool ejecuta)
         {
             match("scanf");
             match("(");
@@ -408,19 +418,23 @@ namespace Sintaxis_2
             }
             string variable = getContenido();
             match(Tipos.Identificador);
-            float captura = float.Parse(Console.ReadLine());
-            Modifica(variable,captura);
+            if (ejecuta)
+            {
+                string captura = "" + Console.ReadLine();
+                float resultado = float.Parse(captura);
+                Modifica(variable,resultado);
+            }
             match(")");
             match(";");
         }
         //Main -> void main() BloqueInstrucciones
-        private void Main()
+        private void Main(bool ejecuta)
         {
             match("void");
             match("main");
             match("(");
             match(")");
-            BloqueInstrucciones();
+            BloqueInstrucciones(ejecuta);
         }
         //Expresion -> Termino MasTermino
         private void Expresion()
@@ -483,7 +497,6 @@ namespace Sintaxis_2
                 {
                     throw new Error("de sintaxis, la variable <" + getContenido() + "> no está declarada", log, linea, columna);
                 }
-
                 match(Tipos.Identificador);
             }
             else
@@ -495,5 +508,3 @@ namespace Sintaxis_2
         }
     }
 }
-
-//otra cosa diferente
